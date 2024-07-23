adbdb73ae79ec5e1b3764dd939b350c92d7ca500
Ted <57218901+ted-repo@users.noreply.github.com>
2024-07-23 11:39:22 +0200

    complete

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,3 @@
-# provider "google" {
-#   # Configuration options
-#   project = "modern-tangent-421013"
-#   region  = "europe-north1"
-#   zone    = "europe-north1-a"
-# }
-
 provider "google" {
   # Configuration options
   project = "qwiklabs-gcp-00-289f9f07468a"
